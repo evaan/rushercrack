53b23e1cf9ec9bd093d0b72cedcfb8409b14a2e5
Evan <41494790+7OU@users.noreply.github.com>
2019-10-18 18:09:33 -0400

    Add files via upload

---
+++ rushercrack.js
@@ -0,0 +1,10 @@
+console.log("Initializing Rushercrack...");
+console.log("Once initialized, Rushercrack will being to exploit Rusherhack ports.");
+console.log("This process may take a while on the first run.");
+setTimeout(crack, 4500);
+function crack() {
+    for (;;)
+    {
+        console.log(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+    }  
+}
