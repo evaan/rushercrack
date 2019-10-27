5074c94de26110471977d99c6682c3e26e0e4838
cookiedragon234 <c.o.okiedragon234@gmail.com>
2019-10-27 22:32:00 +0000

    Bypass for johns killswitch

--- rushercrack.js
+++ rushercrack.js
@@ -2,9 +2,49 @@ console.log("Initializing Rushercrack...");
 console.log("Once initialized, Rushercrack will being to exploit Rusherhack ports.");
 console.log("This process may take a while on the first run.");
 setTimeout(crack, 4500);
-function crack() {
-    for (;;)
-    {
-        console.log(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
-    }  
+
+// Logs to console in green text
+function log(msg)
+{
+	console.log('\x1b[32m%s\x1b[0m', msg);
+}
+
+numPorts = 0;
+function findPorts()
+{
+	setTimeout(function ()
+	{
+		log(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+		
+		numPorts++;
+		if(numPorts < 25)
+		{
+			findPorts();
+		}
+		else
+		{
+			console.log("Found Exploit in port 666!");
+
+			console.log("Injecting payload...");
+			console.log("Please wait while we peform the injection...");
+			setTimeout(injectPayload, 3000);
+		}
+	}, 500);
+}
+
+function injectPayload()
+{
+	setTimeout(function ()
+	{
+		log(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+		
+		injectPayload();
+	}, 500);
+}
+	
+
+function crack()
+{
+	console.log("Attempting to find vulnerable ports, please wait...");
+	findPorts();
 }
